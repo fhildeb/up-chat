3b6e960331052e184283fec85a07af427dfb6f19
Felix Hildebrandt <felix.hildebrandt@gmx.net>
2023-02-25 00:20:20 +0100

    add react router and sample pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import Landing from "./pages/Landing";
+import Onboarding from "./pages/Onboarding";
+import Chat from "./pages/Chat";
+import FAQ from "./pages/FAQ";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Landing} />
+        <Route path="/onboarding" component={Onboarding} />
+        <Route path="/chat" component={Chat} />
+        <Route path="/faq" component={FAQ} />
+      </Switch>
+    </Router>
   );
 }
 
