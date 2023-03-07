b806e6e463f15643606f0bd3123a328c7c2a480b
Felix Hildebrandt <felix.hildebrandt@gmx.net>
2023-03-07 18:46:03 +0100

    add sass and styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.scss';
 
 /**
  * Initializes a React application by rendering
